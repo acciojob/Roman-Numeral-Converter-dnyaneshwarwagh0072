031ffce1a2c46d64ab413c2545993fa4a7e58f24
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-10-14 18:21:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
-
-
 function convertToRoman(num) {
-    const obj = {
-        0: ['M', 1000],
-        1: ['D', 500],
-        2: ['C', 100],
-        3: ['L', 50],
-        4: ['X', 10],
-        5: ['V', 5],
-        6: ['I', 1]
-    };
-
-    let roman = '';
-    const subtractionCases = {
-        900: 'CM',
-        400: 'CD',
-        90: 'XC',
-        40: 'XL',
-        9: 'IX',
-        4: 'IV'
-    };
-
-    for (const key in subtractionCases) {
-        while (num >= key) {
-            roman += subtractionCases[key];
-            num -= key;
-        }
+  const obj = {
+    0: ['M', 1000],
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
+  };
+  
+  let roman = '';
+  for (let i in obj) {
+    while (num >= obj[i][1]) {
+      roman += obj[i][0];
+      num -= obj[i][1];
     }
-    for (let i = 0; i < 7; i++) {
-        while (num >= obj[i][1]) {
-            roman += obj[i][0];
-            num -= obj[i][1];
-        }
-    }
-    return roman;
+  }
+  
+  return roman;
 }
 
-console.log(convertToRoman(14));
-console.log(convertToRoman(798));
 module.exports = convertToRoman;
