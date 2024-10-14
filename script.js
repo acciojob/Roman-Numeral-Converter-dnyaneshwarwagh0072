06bee83d8c79843cace973ad11e303ef9a870565
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-10-14 23:00:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// function convertToRoman(num) {
-//   const obj = {
-//     0: ['M', 1000],
-//     1: ['D', 500],
-//     2: ['C', 100],
-//     3: ['L', 50],
-//     4: ['X', 10],
-//     5: ['V', 5],
-//     6: ['I', 1]
-//   };
-  
-//   let roman = '';
-//   for (let i in obj) {
-//     while (num >= obj[i][1]) {
-//       roman += obj[i][0];
-//       num -= obj[i][1];
-//     }
-//   }
-  
-//   return roman;
-// }
-// console.log(convertToRoman(36));  
-// module.exports = convertToRoman;
-
 function convertToRoman(num) {
 	const obj = {
     0: ['M', 1000],
