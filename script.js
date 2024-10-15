6880c53363da0803c98a85580d82236d1f2bcd24
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-10-15 10:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 function convertToRoman(num) {
-	const obj = {
-    0: ['M', 1000],
-    1: ['D', 500],
-    2: ['C', 100],
-    3: ['L', 50],
-    4: ['X', 10],
-    5: ['V', 5],
-    6: ['I', 1]
-  };
-    let result = '';
-    for (let i = 0; i < romanSymbols.length; i++) {
-        while (num >= romanSymbols[i][1]) {
-            result += romanSymbols[i][0];
-            num -= romanSymbols[i][1];
+    const obj = {
+        0: ['M', 1000],
+        1: ['D', 500],
+        2: ['C', 100],
+        3: ['L', 50],
+        4: ['X', 10],
+        5: ['V', 5],
+        6: ['I', 1]
+    };
+
+    const romanSpecialCases = {
+        900: "CM",
+        400: "CD",
+        90: "XC",
+        40: "XL",
+        9: "IX",
+        4: "IV"
+    };
+
+    let roman = "";
+
+    // Handle special cases like 900, 400, etc.
+    for (const [value, symbol] of Object.entries(romanSpecialCases)) {
+        while (num >= value) {
+            roman += symbol;
+            num -= value;
         }
     }
 
-    return result;
+    // Handle general roman numerals
+    for (let i = 0; i <= 6; i++) {
+        const [symbol, value] = obj[i];
+        while (num >= value) {
+            roman += symbol;
+            num -= value;
+        }
+    }
+
+    return roman;
 }
-console.log(convertToRoman(36));
+
+console.log(convertToRoman(36));  // Output: XXXVI
 module.exports = convertToRoman;
