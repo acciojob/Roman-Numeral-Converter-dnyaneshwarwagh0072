a2cf540b8ead4c2260f79829750973e1b9a9d80a
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-10-15 10:40:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
 function convertToRoman(num) {
-    // Define Roman numeral symbols and their values
-    const romanSymbols = [
-        ["M", 1000],
-        ["CM", 900],
-        ["D", 500],
-        ["CD", 400],
-        ["C", 100],
-        ["XC", 90],
-        ["L", 50],
-        ["XL", 40],
-        ["X", 10],
-        ["IX", 9],
-        ["V", 5],
-        ["IV", 4],
-        ["I", 1]
-    ];
-
+	const obj = {
+    0: ['M', 1000],
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
+  };
     let result = '';
-
-    // Loop through each symbol and value
-    for (let [symbol, value] of romanSymbols) {
-        // Append the symbol while reducing the number
-        while (num >= value) {
-            result += symbol;
-            num -= value;
+    for (let i = 0; i < romanSymbols.length; i++) {
+        while (num >= romanSymbols[i][1]) {
+            result += romanSymbols[i][0];
+            num -= romanSymbols[i][1];
         }
     }
 
     return result;
 }
-
-// Example usage:
-console.log(convertToRoman(14));  // Output: XIV
-console.log(convertToRoman(798)); // Output: DCCXCVIII
+console.log(convertToRoman(36));
+module.exports = convertToRoman;
